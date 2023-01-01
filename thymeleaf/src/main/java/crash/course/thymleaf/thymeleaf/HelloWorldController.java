d209b3851f274affff3143aebbd975ced5ccde36
EON-DC <mun0392@gmail.com>
2023-01-01 15:55:56 +0900

    Initial commit

---
+++ thymeleaf/src/main/java/crash/course/thymleaf/thymeleaf/HelloWorldController.java
@@ -0,0 +1,49 @@
+package crash.course.thymleaf.thymeleaf;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+@Controller
+public class HelloWorldController {
+
+    @GetMapping("/hello")
+    public String hello(Model model) {
+        model.addAttribute("message", "Hello world!");
+        return "helloworld";
+    }
+
+    @GetMapping("/demo")
+    public String demo() {
+        return "add-css-js-demo";
+    }
+
+    @GetMapping("/bootstrap")
+    public String bootstrap() {
+        return "add-bootstrap";
+    }
+
+    @GetMapping("/iteration")
+    public String iter(Model model) {
+        Employee john = new Employee("John", "Martin", "John@email.com");
+        Employee mark = new Employee("Mark", "Heli", "Mark@email.com");
+        Employee sam = new Employee("Sam", "Sung", "Sam@email.com");
+        List<Employee> employees = new ArrayList<>(Arrays.asList(john, mark, sam));
+        model.addAttribute("employees", employees);
+        return "iteration";
+    }
+
+    @GetMapping("/if-unless")
+    public String if_unless(Model model) {
+        User john = new User("John", "John@email.com", "ADMIN");
+        User mark = new User("Mark", "Mark@email.com", "USER");
+        List<User> users = new ArrayList<>(Arrays.asList(john, mark));
+        model.addAttribute("users", users);
+        return "if-unless";
+    }
+}
---
+++ thymeleaf/src/test/java/crash/course/thymleaf/thymeleaf/ThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package crash.course.thymleaf.thymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
